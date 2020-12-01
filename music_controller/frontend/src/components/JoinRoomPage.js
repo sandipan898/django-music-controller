2372ef9530c5fbedc26ebb73d0a87fa2790e9eca
sandipan <sandipan@localhost.localdomain>
2020-12-01 11:40:40 +0530

    adding material ui components

--- music_controller/frontend/src/components/JoinRoomPage.js
+++ music_controller/frontend/src/components/JoinRoomPage.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
-
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default class JoinRoomPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: "",
+            error: ""
+        }
     }
 
     render() {
-        return <p>This is the Join Room Page</p>
+        return (
+            <Grid container spacing={1} alignItems="center">
+                <Grid item xs={12} align="center">
+                    <Typography varient="h4" component="h4">
+                        Join a Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <TextField
+                        error={this.state.error}
+                        label="code"
+                        placeholder="Enter a Room Code"
+                        value={ this.state.roomCode }
+                        helpertext={this.state.error}
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button varient="contained" color="primary" onClick>
+                        Enter Room
+                    </Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button varient="contained" color="secondary" to="/" component={ Link }>
+                        Back
+                    </Button>
+                </Grid>
+            </Grid>
+        )
     }
 }
