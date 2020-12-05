832ee660edec65e852efc24611c6e72e20527ca0
sandipan898 <sandipan.das898@gmail.com>
2020-12-06 01:52:17 +0530

    define UserInRoom View and componentDidMount method

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import JoinRoomView, RoomView, CreateRoomView, GetRoom
+from .views import JoinRoomView, RoomView, UserInRoom, CreateRoomView, GetRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoomView.as_view()),
+    path('user-in-room', UserInRoom.as_view()),
 ]
--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -13,7 +13,20 @@ import { BrowserRouter as Router,
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: null
+        }
     }
+
+    async componentDidMount() {
+        fetch("/api/user-in-room")
+          .then((response) => response.json())
+          .then((data) => {
+            this.setState({
+              roomCode: data.code,
+            });
+          });
+      }
 
     renderHomePage() {
         return (
@@ -38,16 +51,22 @@ export default class HomePage extends Component {
     }
 
     render() {
-        return <Router>
-
-            <Switch>
-                <Route exact path='/'>
-                    {this.renderHomePage()}
-                </Route>
-                <Route path='/join' component={JoinRoomPage}/>
-                <Route path='/create' component={CreateRoomPage}/>
-                <Route path="/room/:roomCode/" component={Room} />
-            </Switch>
-        </Router>;
+        return (
+            <Router>
+                <Switch>
+                    <Route 
+                        exact path='/' 
+                        render ={() => {
+                            return this.state.roomCode ? (
+                                <Redirect to={`/room/${this.state.roomCode}`} />
+                            ) : ( this.renderHomePage() );
+                        }}
+                    />
+                    <Route path='/join' component={JoinRoomPage}/>
+                    <Route path='/create' component={CreateRoomPage}/>
+                    <Route path="/room/:roomCode/" component={Room} />
+                </Switch>
+            </Router>
+        );
     }
 }
